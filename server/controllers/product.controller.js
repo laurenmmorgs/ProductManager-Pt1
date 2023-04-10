4ad7230b00b7e8bbe04ca8e22efe24f1fe1c364a
Lauren Morgan <lauren.lolo.morgan@gmail.com>
2023-04-09 17:11:01 -0700

    first commit:

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,23 @@
+const Product = require('../models/product.model');    /* this is new */
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+          /* The method below is new */
+module.exports.createProduct = (request, response) => {
+    // Mongoose's "create" method is run using our Product model to add a new product to our db's product collection.
+    // request.body will contain something like {firstName: "Billy", lastName: "Washington"} from the client
+    Product.create(request.body) //This will use whatever the body of the client's request sends over
+        .then(product => response.json(product))
+        .catch(err => response.json(err));
+}
+module.exports.findAllProducts = (req, res) => {
+    Product.find()
+        .then((allDaProducts) => {
+            res.json({ products: allDaProducts })
+        })
+        .catch((err) => {
+            res.json({ message: 'Something went wrong', error: err })
+        });
+}
